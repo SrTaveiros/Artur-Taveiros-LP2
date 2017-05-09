e95811f11c59f5419eceb3771412e0841233c616
SrTaveiros <artur.taveiros@live.com>
2017-05-09 13:27:31 -0300

    correção

--- ExercicioRevisao/ExercicioRevisao/Program.cs
+++ ExercicioRevisao/ExercicioRevisao/Program.cs
@@ -10,15 +10,20 @@ namespace ExercicioRevisao
     {
         static void Main(string[] args)
         {
-            int qtd;
-            Console.WriteLine("informe a quantidade de casa");
+            int qtd, pot;
+            string nome;
+            double quilm;
+            Console.WriteLine("informe a quantidade de carros");
             qtd = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < qtd; i++)
             {
-                string nome = Console.ReadLine();
-                double quilometragem = double.Parse(Console.ReadLine());
-                int potencia = int.Parse(Console.ReadLine());
+                Console.writeline("informe o modelo do carro");
+                nome = Console.ReadLine();
+                Console.WriteLine("informe a quiometragem do veiculo");
+                quilm = double.Parse(Console.ReadLine());
+                Console.WriteLine("informe a potencia do veiculo");
+                pot = int.Parse(Console.ReadLine());
 
                 Console.WriteLine(Classificar(nome, quilometragem, potencia));
             }
@@ -44,6 +49,5 @@ namespace ExercicioRevisao
 
             return String.Format("{0} - {1} - {2}", mod, classifRodagem, classifPot);
         }
-    }
     }
 }
