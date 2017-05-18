d2b115a230e54d6f18636d2c4403b9af56202b0d
SrTaveiros <artur.taveiros@live.com>
2017-05-18 14:12:23 -0300

    DiaDoAno

---
+++ DiaDoAno/DiaDoAno/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiaDoAno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mes;
+            int data, dia;
+
+            Console.WriteLine("informe o dia");
+            mes = Console.ReadLine();
+
+
+        }
+    }
+}
